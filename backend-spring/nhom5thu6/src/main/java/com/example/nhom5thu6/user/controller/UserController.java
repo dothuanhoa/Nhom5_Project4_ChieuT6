8633a8ced029bb312a4fc32a82184a8252c775c3
Thuan Hoa <dothuanhoa1811@gmail.com>
2026-04-10 13:25:15 +0700

    sửa mapping

--- backend-spring/nhom5thu6/src/main/java/com/example/nhom5thu6/user/controller/UserController.java
+++ backend-spring/nhom5thu6/src/main/java/com/example/nhom5thu6/user/controller/UserController.java
@@ -8,22 +8,15 @@ import com.example.nhom5thu6.user.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService service;
